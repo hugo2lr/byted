0f55ac8b7b070c2fa8a1e63a87c2aacf880f4283
hugo2lr <84332699+hugo2lr@users.noreply.github.com>
2021-06-07 14:35:36 +0200

    changement de index.html en index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>byTed -> Le meilleur textile de France !</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <header>
+    <img src="img/logo.png"></img>
+    <p>Teddy millionaire</p>
+  </header>
+  
+</body>
+</html>
